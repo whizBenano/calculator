e8ac8421d1d627015450e572bc71fd97e6ccad04
whizBenano <benanspencer@gmail.com>
2023-03-12 11:12:50 +0100

    fixed backspace, decimal key and evaluation functionalities

--- calculator.js
+++ calculator.js
@@ -8,55 +8,77 @@ let btn = document.querySelector("#myBtn");
 let span = document.querySelector(".close");
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+btn.onclick = () => modal.style.display = "block"
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = () => modal.style.display = "none"
 
-
+// get the elements
 let screen = document.querySelector('#display')
 let backSpace = document.querySelector('.del')
 let clearScreen = document.querySelector('.clear')
-
-let temporary = ''
-let final
 
 let plus = document.querySelector('.plus')
 let minus = document.querySelector('.minus')
 let times = document.querySelector('.times')
 let divide = document.querySelector('.divide')
+let equal =  document.querySelector('.equal')
 
 screen.innerHTML = '0'
 
 // show numbers on calculator screen
-let keys = document.querySelectorAll('button')
-keys.forEach(key => {
-  key.addEventListener('click', () => {
-    screen.innerHTML === '0' ? 
-      screen.innerHTML = key.value : 
-      screen.innerHTML += key.value
-    
-    temporary = screen.innerHTML
-  })
-})
+let keys = document.querySelectorAll('.operand')
+let buttons = document.querySelectorAll('button')
+let operators =  document.querySelectorAll('.operator')
 
 // clear the entire screen
 clearScreen.onclick = () => screen.innerHTML = '0'
 
+// oprations
+let temporary = ''
+let finale
+
 // remove the last number
 backSpace.onclick = () => {
-  screen.innerHTML = screen.innerHTML.length === 0 ? '0' : screen.innerHTML
-  screen.innerHTML = screen.innerHTML.substr(1)
+  screen.innerHTML = screen.innerHTML.length < 2 ? '0' : screen.innerHTML.substr(0, screen.innerHTML.length - 1)
+  temporary = temporary.substr(0, temporary.length - 1)
 }
 
-// plus action
-plus.onclick = () => {
+keys.forEach(key => {
+  key.addEventListener('click', () => {
 
-  temporary = parseInt(temporary)
+    screen.innerHTML === '0' && key.value === '.' ?
+    screen.innerHTML += key.value :
+    screen.innerHTML === '0' ?
+    screen.innerHTML = key.value :
+    screen.innerHTML.includes('.') ?
+    screen.innerHTML = screen.innerHTML + key.value.replace('.', '') :
+    screen.innerHTML += key.value
 
-  final = 0
+  })
+})
 
-  console.log(typeof(temporary))
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+
+    button.classList.contains('operand') ? 
+    temporary += parseInt(button.value) :
+    button.classList.contains('operator') ?
+    temporary += button.value :
+    button.classList.contains('equal') ?
+    screen.innerHTML = eval(temporary) :
+    screen.innerHTML = '0'
+
+    console.log(temporary)
+  })
+})
+
+// operators.forEach(operator => {
+//   operator.addEventListener('click', () => {
+//     screen.innerHTML.replace(screen.innerHTML, key.value)
+//   })
+// })
+
+equal.onclick = () => {
+  // screen.innerHTML = screen.innerHTML === '0' ? '0'
 }
